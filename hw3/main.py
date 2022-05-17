98da1cd231a26e7c2302a910d7cac3472e8a49c2
ChenxiZhou0619 <1305845549@qq.com>
2022-05-17 20:03:39 +0800

    compute the confusion_matrix

--- hw3/main.py
+++ hw3/main.py
@@ -3,6 +3,8 @@ import sklearn.preprocessing, sklearn.decomposition
 from sklearn.model_selection import train_test_split
 from sklearn.utils import column_or_1d
 from sklearn_pandas import DataFrameMapper
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, precision_score, confusion_matrix
 import numpy as np
 
 import torch
@@ -18,6 +20,7 @@ def pre_process_data():
     pri_star_table = pandas.read_csv (
         "/mnt/2022spr/data-integrate/data/pri_star_info_202205141729.csv"
     )
+    
     cust_asset = pandas.read_csv(
         "/mnt/2022spr/data-integrate/data/pri_cust_asset_info_202205141729.csv",
         usecols=[
@@ -29,24 +32,61 @@ def pre_process_data():
             "td_bal",
             "fin_bal",
             "sa_crd_bal",
-            "sa_td_bal",
-            "ntc_bal",
-            "td_3m_bal",
-            "cd_bal",
-            "etl_dt"
+            "etl_dt",
+            "sa_td_bal"
         ]
     )
 
+    cust_asset_acct = pandas.read_csv (
+        "/mnt/2022spr/data-integrate/data/pri_cust_asset_acct_info_202205141729.csv",
+        usecols=[
+            "uid",
+            "subject_no",   #OrdinalEncoder
+            "prod_type",    #same
+            "acct_char",    #same
+            "frz_sts",      #same
+            "stp_sts",      #same
+            "acct_bal"     #standardScaler
+        ]
+    )
+
+#    pri_cust_base = pandas.read_csv (
+#        "/mnt/2022spr/data-integrate/data/pri_cust_base_info_202205141729.csv",
+#        usecols=[
+#            'uid',
+#            'marrige',
+#            'education',
+#            'career',
+#            'prof_titl',
+#            'is_employee',
+#            'is_shareholder',
+#            'is_black'
+#        ]
+#    )
+
+    # merge 1
     res = pandas.merge(pri_star_table, cust_asset, how='left', on='uid')
     res = res[(res['star_level']!=-1)]
     res = res[(res['all_bal'].notnull())]
+    
+    # merge 2
+    res = pandas.merge(res, cust_asset_acct, how="left", on="uid")
+    res = res[(res['subject_no'].notnull())]
+
+#    # merge 3
+#    res = pandas.merge(res, pri_cust_base, how='left', on='uid')
+#    res = res[(res['marrige'].notnull())]
 
     train_mapper = DataFrameMapper ([
         (['all_bal'], sklearn.preprocessing.StandardScaler()),
         (['avg_mth'], sklearn.preprocessing.StandardScaler()),
         (['avg_year'], sklearn.preprocessing.StandardScaler()),
         (['sa_bal'], sklearn.preprocessing.StandardScaler()),
+        (['td_bal'], sklearn.preprocessing.StandardScaler()),
+        (['fin_bal'], sklearn.preprocessing.StandardScaler()),
         (['sa_crd_bal'], sklearn.preprocessing.StandardScaler()),
+        (['acct_bal'], sklearn.preprocessing.StandardScaler()),
+
     ])
 
     X = np.round (train_mapper.fit_transform(res.copy()), 2)
@@ -79,10 +119,10 @@ class MyNetwork(nn.Module):
 
         #self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential (
-            nn.Linear(5, 20),
+            nn.Linear(8, 20),
             nn.SELU(),
             nn.Linear(20, 10),
-            nn.SELU()
+            nn.SELU(),
         )
     def forward(self, x):
         #x = self.flatten(x)
@@ -126,27 +166,33 @@ def test_loop(dataloader, model, loss_fn):
 if __name__ == "__main__":
 
     train_x, train_y, test_x, test_y = pre_process_data()
+
+    clf = DecisionTreeClassifier()
+
+    clf.fit(train_x, train_y)
+    pred = clf.predict(test_x)
+    print(confusion_matrix(test_y, pred))
     
-    train_data = MyDataset(train_x, train_y)
-    test_data = MyDataset(test_x, test_y)
-
-    train_data_loader = DataLoader (
-        train_data, batch_size=64
-    )
-    test_data_loader = DataLoader (
-        test_data, batch_size=64
-    )
-
-    model = MyNetwork()
-    loss_fn = nn.CrossEntropyLoss()
-    learning_rate = 5e-3
-    optimizer = torch.optim.ASGD(model.parameters(), lr=learning_rate)
-
-    epochs = 10
-    for t in range(epochs):
-        print(f"Epoch {t+1}\n-------------------------------")
-        train_loop(train_data_loader, model, loss_fn, optimizer)
-        test_loop(test_data_loader, model, loss_fn)
-    print("Done!")
+    #train_data = MyDataset(train_x, train_y)
+    #test_data = MyDataset(test_x, test_y)
+#
+    #train_data_loader = DataLoader (
+    #    train_data, batch_size=64
+    #)
+    #test_data_loader = DataLoader (
+    #    test_data, batch_size=64
+    #)
+#
+    #model = MyNetwork()
+    #loss_fn = nn.CrossEntropyLoss()
+    #learning_rate = 1e-3
+    #optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+#
+    #epochs = 10
+    #for t in range(epochs):
+    #    print(f"Epoch {t+1}\n-------------------------------")
+    #    train_loop(train_data_loader, model, loss_fn, optimizer)
+    #    test_loop(test_data_loader, model, loss_fn)
+    #print("Done!")
 
   
